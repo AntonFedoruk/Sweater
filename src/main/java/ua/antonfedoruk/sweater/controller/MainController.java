831cb1afab0bcfdab3be9d4ce46e9245abefdd57
Anton Fedoruk <antobn.fedoruk@gmail.com>
2020-12-24 20:31:01 +0200

    Added:  - moved to freemarker template engine to replace mustache.

--- src/main/java/ua/antonfedoruk/sweater/controller/MainController.java
+++ src/main/java/ua/antonfedoruk/sweater/controller/MainController.java
@@ -3,7 +3,10 @@ package ua.antonfedoruk.sweater.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ua.antonfedoruk.sweater.model.Message;
 import ua.antonfedoruk.sweater.model.User;
 import ua.antonfedoruk.sweater.repository.MessageRepository;
@@ -22,10 +25,17 @@ public class MainController {
     }
 
     @GetMapping("/messages")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false) String filter, Model model) {
         Iterable<Message> messages = messageRepository.findAll();
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
 
         return "main";
     }
@@ -44,25 +54,9 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("/filter")
-    public String filter(@RequestParam String filter,
-                         Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-
-        model.put("messages", messages);
-
-        return "main";
-    }
-
-    @DeleteMapping("/messages/{tag}")
-    public String deleteByTag(@PathVariable String tag) {
-        messageRepository.deleteByTag(tag);
-        return "redirect:/messages";
-    }
+//    @DeleteMapping("/messages/{tag}")
+//    public String deleteByTag(@PathVariable String tag) {
+//        messageRepository.deleteByTag(tag);
+//        return "redirect:/messages";
+//    }
 }
